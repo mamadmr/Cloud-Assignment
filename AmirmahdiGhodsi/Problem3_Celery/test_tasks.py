97b89a751a3c702465fa3cdf84b4a33510ba47ab
Amirmehdig <amirmahdig138@gmail.com>
2025-05-11 21:11:08 +0330

    Finish phase3

---
+++ AmirmahdiGhodsi/Problem3_Celery/test_tasks.py
@@ -0,0 +1,11 @@
+from tasks import start_container, stop_container
+
+if __name__ == "__main__":
+    # Start a container
+    start_result = start_container.delay("pasapples/apjctf-todo-java-app:latest", "test_redis_ctf")
+    cid = start_result.get(timeout=20)
+    print("Started =>", cid)
+
+    # Stop the same container
+    stop_result = stop_container.delay("test_redis_ctf")
+    print(stop_result.get(timeout=20))
