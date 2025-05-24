17b4cb415007d55054765c3d25c22a7a8a18b50c
Semir <seyedmojtabamirsoleimani@gmail.com>
2025-05-24 09:13:55 +0330

    DockerCompose for Problem5

---
+++ Seyed_Mojtaba_Mirsoleimani/Problem5_DockerCompose/app/docker_utils.py
@@ -0,0 +1,19 @@
+import docker
+
+client = docker.from_env()
+
+def start_container(team_id, challenge_id):
+    name = f"{team_id}_{challenge_id}"
+    container = client.containers.run(
+        "challenge_image",  
+        detach=True,
+        name=name,
+        ports={"80/tcp": None}
+    )
+    ip = container.attrs["NetworkSettings"]["IPAddress"]
+    return container.id, ip
+
+def stop_container(container_id):
+    container = client.containers.get(container_id)
+    container.stop()
+    container.remove()
